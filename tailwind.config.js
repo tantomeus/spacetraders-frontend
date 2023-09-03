ddb79ad7f930c6c8c571f5771aba5e4f6d80d4fc
Stormblessed <carlomanid@gmail.com>
2023-09-03 19:59:56 +0300

    Add animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+// @type {import('tailwindcss').Config}
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,7 +9,29 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        planet: {
+          "100%" : {
+            backgroundPosition: "-12000px",
+          }
+        },
+        sunX: {
+          "100%": {
+            backgroundPositionX: "-12000px",
+          }
+        },
+        sunY: {
+          "100%": {
+            backgroundPositionY: "-800px",
+          },
+        }
+      },
+      animation: {
+        "planet": "planet 12s steps(120) infinite",
+        "sun": "sunX 6s steps(60) infinite, sunY 24s steps(4) infinite"
+      }
+    },
   },
   plugins: [],
 }
