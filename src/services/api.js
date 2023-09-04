5aa5c4c4935b66833c9ef6fa3223c38bd8a5ca75
Stormblessed <carlomanid@gmail.com>
2023-09-04 22:10:39 +0300

    Add more APIs

--- src/services/api.js
+++ src/services/api.js
@@ -11,11 +11,12 @@ export async function signUp(callsign) {
       };
 
     const res = await fetch("https://api.spacetraders.io/v2/register", options);
-    const data = await res.json();
+    const { data } = await res.json();
+    console.log(data)
     return data;
 }
 
-export async function auth(token) {
+export async function getAgent(token) {
   const options = {
     headers: {
       "Content-Type": "application/json",
@@ -24,11 +25,12 @@ export async function auth(token) {
   };
     
   const res = await fetch("https://api.spacetraders.io/v2/my/agent", options);
-  const data = await res.json();
+  const { data } = await res.json();
+  console.log(data)
   return data;
 }
 
-export async function getSystems() {
+export async function getSystems(token) {
   const options = {
     headers: {
       "Content-Type": "application/json",
@@ -37,6 +39,63 @@ export async function getSystems() {
   };
   
   const res = await fetch("https://api.spacetraders.io/v2/systems", options);
-  const data = await res.json();
+  const { data } = await res.json();
+  console.log(data)
+  return data;
+}
+
+export async function getWaypoints(token, system) {
+  const options = {
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`
+    },
+  };
+  
+  const res = await fetch(`https://api.spacetraders.io/v2/systems/${system}/waypoints`, options);
+  const { data } = await res.json();
+  console.log(data)
+  return data;
+}
+
+export async function getContracts(token, system) {
+  const options = {
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`
+    },
+  };
+  
+  const res = await fetch('https://api.spacetraders.io/v2/my/contracts', options);
+  const { data } = await res.json();
+  console.log(data)
+  return data;
+}
+
+export async function acceptContract(token, id) {
+  const options = {
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`
+    },
+  };
+  
+  const res = await fetch(`https://api.spacetraders.io/v2/my/contracts/${id}/accept`, options);
+  const { data } = await res.json();
+  console.log(data)
+  return data;
+}
+
+export async function getShips(token) {
+  const options = {
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`
+    },
+  };
+  
+  const res = await fetch('https://api.spacetraders.io/v2/my/ships', options);
+  const { data } = await res.json();
+  console.log(data)
   return data;
 }
