66ca4007825e8f595dac78af737c4d2aa50c4762
Stormblessed <carlomanid@gmail.com>
2023-09-08 20:05:56 +0300

    Fix markup

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,12 +1,13 @@
-import { getPlanetStyles } from "@/helpers/getPlanetStyles";
+import PlanetItem from "./PlanetItem";
 
 export default function Planets({ waypoints = [] }) {
     return <ul className="flex justify-around flex-wrap px-5 pb-5 text-center">
         {waypoints.map((point) => {
-            const { bg, animation, scale, size, m } = getPlanetStyles(point.type);
 
-            return <li className="flex flex-col justify-end" key={point.symbol}><div style={{transformOrigin: "center", transform: scale, height: size, width: size}} className={`${m} ${animation} ${bg}`}></div>
-            <span className="text-xs">{point.symbol}</span></li>
+            return <li className="flex flex-col justify-end" key={point.symbol}>
+                <PlanetItem type={point.type}/>
+                <span className="text-xs">{point.symbol}</span>
+            </li>
         })}
     </ul>
 }
