edf1ae94cc87dd11728196e7d98ac1c79f3b7e91
Stormblessed <carlomanid@gmail.com>
2023-09-08 20:05:19 +0300

    Add handlers

--- src/components/ContractItem.js
+++ src/components/ContractItem.js
@@ -1,10 +1,19 @@
+"use client";
+
 import { acceptContract } from "@/services/api";
+import { useState } from "react";
 
 export default function ContractItem({ contract, token }) {
+    const [isAccepted, setIsAccepted] = useState(false);
 
     function formatDate(date) {
         const formated =  date.split("T")[0].split("-");
         return `${formated[2]}.${formated[1]}.${formated[0]}`;
+    }
+
+    async function handleAccept(token, contract) {
+        const data = await acceptContract(token, contract);
+        setIsAccepted(true);
     }
 
     return <li className="bg-stone-900 p-5 rounded-3xl">
@@ -22,7 +31,7 @@ export default function ContractItem({ contract, token }) {
             <div><h3 className="text-xs opacity-50">destination</h3>{contract.terms.deliver[0].destinationSymbol}</div>
             
             {
-            contract.accepted && <>
+            contract.accepted || isAccepted && <>
             <div><h3 className="text-xs opacity-50">units require</h3>{contract.terms.deliver[0].unitsRequired}</div>
             <div><h3 className="text-xs opacity-50">units fulfilled</h3>{contract.terms.deliver[0].unitsFulfilled}</div>
             </>
@@ -32,9 +41,9 @@ export default function ContractItem({ contract, token }) {
             <div><h3 className="text-xs opacity-50">fulfill</h3>{contract.terms.payment.onFulfilled} credits</div>
         </div>
         <div className="flex items-center">
-            <div>{contract.accepted ? formatDate(contract.terms.deadline) : formatDate(contract.expiration)}</div>
+            <div>{contract.accepted || isAccepted ? formatDate(contract.terms.deadline) : formatDate(contract.expiration)}</div>
             <div className="grow px-3"><hr className="opacity-50"/></div>
-            {contract.accepted ? <span className="text-amber-600 text-xl p-2">&#10003; accepted</span> : <button onClick={() => acceptContract(token, contract.id)} className="bg-amber-600 text-xl p-2">Accept</button>}
+            {contract.accepted || isAccepted ? <span className="text-amber-600 text-xl p-2">&#10003; accepted</span> : <button onClick={() => handleAccept(token, contract.id)} className="bg-amber-600 text-xl p-2">Accept</button>}
         </div>
     </li>
 }
