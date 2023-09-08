a3da98f61492191e95b4735cfb0fd19d1e541fda
Stormblessed <carlomanid@gmail.com>
2023-09-08 10:46:39 +0300

    Create footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+export default function Footer() {
+
+    return <footer className="mt-12 flex justify-center gap-5 bg-stone-900 p-4">
+        <span>
+            Created by <a className="underline hover:text-amber-600" href="https://docs.spacetraders.io/">SpaceTraders API</a>
+        </span>
+        <span>|</span>
+        <span>
+            GitHub <a className="underline hover:text-amber-600" href="https://github.com/tantomeus">Tantomeus</a>
+        </span>
+    </footer>
+}
