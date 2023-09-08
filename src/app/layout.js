c415548721d70f22af75d1a8595fcd7834200816
Stormblessed <carlomanid@gmail.com>
2023-09-08 20:04:14 +0300

    Add footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import './globals.css';
 import { Inter } from 'next/font/google';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AccountProvider from '@/context/AccountContext';
+import Footer from '@/components/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -24,13 +25,14 @@ const queryClient = new QueryClient({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} text-stone-50 bg-stone-950`}>
+      <body className={`${inter.className} relative text-stone-50 bg-stone-950`}>
         <AccountProvider>
           <QueryClientProvider client={queryClient}>
             <Header/>
-            <main className="max-w-7xl mx-auto ">
+            <main className="max-w-7xl mx-auto min-h-[80vh]">
               {children}
             </main>
+            <Footer/>
           </QueryClientProvider>
         </AccountProvider>
       </body>
