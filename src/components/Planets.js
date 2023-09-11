c28efe02ce67f762d27b39ee20d387ed800f1bf4
Stormblessed <carlomanid@gmail.com>
2023-09-11 23:26:06 +0300

    Add text for traits of planets

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -4,9 +4,20 @@ export default function Planets({ waypoints = [] }) {
     return <ul className="flex justify-around flex-wrap px-5 pb-5 text-center">
         {waypoints.map((point) => {
 
-            return <li className="flex flex-col justify-end" key={point.symbol}>
+            const hasShipyard = point.traits?.find(item => item.symbol === "SHIPYARD");
+            const hasMarketplace = point.traits?.find(item => item.symbol === "MARKETPLACE");
+
+            return <li className="flex flex-col justify-start" key={point.symbol}>
                 <PlanetItem type={point.type}/>
-                <span className="text-xs">{point.symbol}</span>
+                <span className="text-[0.6rem]">{point.symbol}</span>
+                <div className="flex flex-col justify-center gap-2 mt-2">
+                    {!!hasShipyard && <div className="text-xs">
+                        shipyard
+                    </div>}
+                    {!!hasMarketplace && <div className="text-xs">
+                        market
+                    </div>}
+                </div>
             </li>
         })}
     </ul>
