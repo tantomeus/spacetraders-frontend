933d6990cf2b3d098449eb9b721bbfb6139d2122
Stormblessed <carlomanid@gmail.com>
2023-09-12 23:21:16 +0300

    Create component

---
+++ src/components/FlightWindow.js
@@ -0,0 +1,12 @@
+"use client";
+
+import { useAccount } from "@/context/AccountContext";
+import { flyToWaypoint, switchFlightMode } from "@/services/api"
+import { useState } from "react";
+
+export default function FlightWindow({ ship }) {
+
+    return <div className="z-[1000] divide-y divide-stone-500 absolute top-[5vh] left-1/2 transform -translate-x-1/2 bg-stone-900 w-[600px] pt-3">
+        
+    </div>
+}
