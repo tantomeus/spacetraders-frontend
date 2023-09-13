ba99b9588ffa2805e51ebac7b18f79d76038911c
Stormblessed <carlomanid@gmail.com>
2023-09-13 23:32:59 +0300

    Add ship icon and fix button

--- src/components/ShipyardItem.js
+++ src/components/ShipyardItem.js
@@ -4,6 +4,7 @@ import { useAccount } from "@/context/AccountContext";
 import { purchaseShip } from "@/services/api";
 import { useState } from "react";
 import { AiOutlineDown, AiOutlineUp } from "react-icons/ai"
+import ShipImg from "./ShipImg";
 
 export default function ShipyardItem({ ship, waypoint }) {
     const { account, setAccount } = useAccount();
@@ -18,10 +19,11 @@ export default function ShipyardItem({ ship, waypoint }) {
         <div className="border-solid border border-stone-500 rounded-md text-sm p-4 space-y-4">
 
             <div className="flex justify-between items-center gap-2">
-                <div></div>
-                <h3>{ship.name}</h3>
-                <span></span>
+                <ShipImg ship={ship}/>
+                <h3 className="text-lg">{ship.name}</h3>
             </div>
+
+            <hr className="grow opacity-50"/>
 
             <div className="flex flex-col items-start">
                 <p className={descriptionHidden ? "overflow-hidden h-5" : ""}>{ship.description}</p>
@@ -55,7 +57,7 @@ export default function ShipyardItem({ ship, waypoint }) {
             <div className="flex justify-between items-center gap-2">
                 <span className="bg-stone-500 text-xs/[1rem] p-2 rounded-md">{ship.purchasePrice} CREDITS</span>
                 <hr className="block grow opacity-50"/>
-                <button onClick={() => handlePurchase(account.token, ship.type, waypoint, ship.purchasePrice)} className="bg-amber-600 text-xs/[1rem] p-2 rounded-md">PURCHASE</button>
+                <button onClick={() => handlePurchase(account.token, ship.type, waypoint, ship.purchasePrice)} className="btn-color hover:btn-color-hover text-xs/[1rem]">PURCHASE</button>
             </div>
         </div>
     </li>
