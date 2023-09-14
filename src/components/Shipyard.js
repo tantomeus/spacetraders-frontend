c2606ecf197ad5a16e04c28a03b557862bf4c215
Stormblessed <carlomanid@gmail.com>
2023-09-14 23:32:22 +0300

    Change styles

--- src/components/Shipyard.js
+++ src/components/Shipyard.js
@@ -17,9 +17,9 @@ export default function Shipyard({ system, waypoint }) {
         fetching();
     }, [account.token, system, waypoint]);
 
-    return <div className="z-[1000] divide-y divide-stone-500 absolute top-[5vh] left-1/2 transform -translate-x-1/2 bg-stone-900 w-[50rem] pt-3">
-        <div className="flex justify-between items-center px-6 pb-4">
-            <h2 className="text-2xl">Ship Market</h2>
+    return <div className="fixed z-[1000] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-stone-900 flex flex-col w-[60rem] divide-y divide-stone-500">
+        <div className="flex justify-between items-center px-6 py-4">
+            <h2 className="text-2xl">Shipyard</h2>
         </div>
 
         <ul className="overflow-auto max-h-[70vh] pt-4 p-4 text-xs grid grid-cols-2 gap-4">
