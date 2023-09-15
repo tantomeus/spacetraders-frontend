bf3a310726068e8447bb0f4b4d3e9fe6b875054a
Stormblessed <carlomanid@gmail.com>
2023-09-15 23:51:35 +0300

    Fix conditions and handlers

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,27 +7,28 @@ import Login from "./Login";
 import { useAccount } from "@/context/AccountContext";
 import AccountInfo from "./AccountInfo";
 import Overlay from "./Overlay";
+import { shorten } from "@/helpers/helpers";
 
-const pages = ["Ships", "Systems", "Market", "Contracts"]
+const pages = ["Ships", "Systems", "Contracts", "Guide"];
 
 export default function Header() {
-    const { account, setAccount } = useAccount();
+    const { account } = useAccount();
     const [isDropDownOpen, setIsDropDownOpen] = useState(false);
-    const [isAccountInfoOpen, setIsAccountInfoOpen] = useState(false);
+    const [openedWindow, setOpenedWindow] = useState(false); // accountInfo, login
+
     const ref = useRef();
 
     function handleOpenDropdown() {
         setIsDropDownOpen((value) => !value);
     }
 
-    function handleOpenAccount() {
+    function handleOpenAccountInfo() {
         setIsDropDownOpen(false);
-        setIsAccountInfoOpen(true);
+        setOpenedWindow("accountInfo");
     }
 
     function handleOpenLogin() {
-        setAccount({});
-        console.log(!!Object.keys(account).length);
+        setOpenedWindow("login");
         setIsDropDownOpen(false);
     }
 
@@ -46,33 +47,33 @@ export default function Header() {
 
     return <header className="mb-5 bg-stone-900 flex items-center justify-between text-stone-100 px-10">
         <div className="flex items-center gap-14">
-            <Image src="/logo.svg"height={68}  width={68} alt="logo"/>
-            <nav className="flex gap-5">
-                {pages.map(page => <Link key={page} href={`/${page.toLowerCase()}`} className="text-xl">{page}</Link>)}
+            <Image src="/logo.svg" height={68}  width={68} alt="logo" className="scale-90"/>
+            <nav className="flex">
+                {pages.map(page => <Link key={page} href={`/${page.toLowerCase()}`} className="text-xl hover:bg-stone-800 px-4 py-2 rounded-md">{page}</Link>)}
             </nav>
         </div>
         <div className="flex items-center space-x-4">
-            <span className="bg-stone-700 rounded-3xl p-2 text-xl">{account.credits} credits</span>
+            <span className="text-stone-700 bg-amber-400 rounded-3xl py-2 px-4 text-xl">{shorten(account.credits)} credits</span>
             <div ref={ref} className="relative">
                 <button data-testid="account" onClick={handleOpenDropdown} className="btn-color hover:btn-color-hover text-xl">{account.name || "heh"}</button>
 
                 {isDropDownOpen && <div className="z-50 rounded-lg absolute right-0 top-12 flex flex-col w-28 bg-stone-700 divide-y divide-stone-500 overflow-hidden">
-                    <button onClick={handleOpenAccount} className="text-xs text-left p-3 hover:bg-stone-600">View account</button>
+                    <button onClick={handleOpenAccountInfo} className="text-xs text-left p-3 hover:bg-stone-600">View account</button>
                     <button onClick={handleOpenLogin} className="text-xs text-left p-3 hover:bg-stone-600">Switch account</button>
                 </div>}
 
             </div>
         </div>
 
-        {isAccountInfoOpen && <>
+        {openedWindow === "accountInfo" && <>
             <AccountInfo token={account.token}/>
-            <Overlay onClose={setIsAccountInfoOpen}/>
+            <Overlay onClose={setOpenedWindow}/>
         </>
         }
 
-        {!Object.keys(account).length && <>
-            <Login />
-            <div className="absolute bg-stone-950 inset-0"></div>
+        {(!Object.keys(account).length || openedWindow === "login") && <>
+            <Login onClose={setOpenedWindow}/>
+            <div className="z-[500] absolute bg-stone-950 inset-0"></div>
         </>}
     </header>
 }
