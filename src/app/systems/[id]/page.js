9913cc96977b470b3925abc29c33f4f78eb346d5
Stormblessed <carlomanid@gmail.com>
2023-09-16 23:04:55 +0300

    Change the display of ships during the transition

--- src/app/systems/[id]/page.js
+++ src/app/systems/[id]/page.js
@@ -11,10 +11,27 @@ export default function System({ params }) {
   const [system, setSystem] = useState([]);
   const [ships, setShips] = useState([]);
 
-  const uniqueWaypoints = [...new Set(ships.map(ship => ship.nav.status === "IN_TRANSIT" ? "IN_TRANSIT" : ship.nav.waypointSymbol))];
-  const collectionOfShips = uniqueWaypoints.map(waypoint => (waypoint === "IN_TRANSIT" ? 
-  {waypoint: {name: waypoint,}, ships: ships.filter(ship => ship.nav.status === "IN_TRANSIT")} : 
-  {waypoint: {name: waypoint, type: system.find(planet => planet.symbol === waypoint)?.type}, ships: ships.filter(ship => ship.nav.waypointSymbol === waypoint && ship.nav.status !== "IN_TRANSIT")}));
+  const uniqueWaypoints = [...new Set(ships.map(ship => ship.nav.status === "IN_TRANSIT" ? "" : ship.nav.waypointSymbol))].filter((item) => item.includes(params.id));
+
+  const collectionOfShips = uniqueWaypoints.map(waypoint => ({
+      waypoint: {name: waypoint, type: system.find(planet => planet.symbol === waypoint)?.type},
+      ships: ships.filter(ship => ship.nav.waypointSymbol === waypoint && ship.nav.status !== "IN_TRANSIT")
+  }));
+
+  const transit = [
+     {
+      waypoint: {type: "inside", name: "IN_TRANSIT"},
+      ships: ships.filter(ship => ship.nav.status === "IN_TRANSIT" && params.id === ship.nav.route.departure.systemSymbol && params.id === ship.nav.route.destination.systemSymbol)
+    },
+     {
+      waypoint: {type: "from", name: "IN_TRANSIT"},
+      ships: ships.filter(ship => ship.nav.status === "IN_TRANSIT" && params.id === ship.nav.route.departure.systemSymbol && params.id !== ship.nav.route.destination.systemSymbol)
+    },
+     {
+      waypoint: {type: "to", name: "IN_TRANSIT"},
+      ships: ships.filter(ship => ship.nav.status === "IN_TRANSIT" && params.id !== ship.nav.route.departure.systemSymbol && params.id === ship.nav.route.destination.systemSymbol)
+    },
+  ]
 
   useEffect(() => {
     async function fetching() {
@@ -26,14 +43,16 @@ export default function System({ params }) {
     fetching();
   }, [params.id, account.token, account.credits, rerender]);
   
-  return <section>
-    <div>
-      <Planets waypoints={system}/>
-    </div>
-    <div className="flex justify-between mt-10">
-      <h2 className="text-6xl font-bold">{params.id}</h2>
-    </div>
-    <ul className="grid grid-cols-2 gap-6 mt-10">
+  return <section className="space-y-10">
+    <Planets waypoints={system}/>
+
+    <h2 className="text-6xl font-bold">{params.id}</h2>
+
+    <ul className="grid grid-cols-3 gap-6">
+      {transit.map(({waypoint, ships}) => <WaypointItem system={system} params={params} ships={ships} waypoint={waypoint} key={waypoint.type}/>)}
+    </ul>
+    {!collectionOfShips.length && <p className="text-center text-2xl">No ships in orbit or docked</p>}
+    <ul className="grid grid-cols-2 gap-6">
       {collectionOfShips.map(({waypoint, ships}) => <WaypointItem system={system} params={params} ships={ships} waypoint={waypoint} key={waypoint.name}/>)}
     </ul>
   </section>
