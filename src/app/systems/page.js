84062f50fd180307cb7a3f53bbdd3a018eaf9a22
Stormblessed <carlomanid@gmail.com>
2023-09-16 22:49:40 +0300

    Add pagination

--- src/app/systems/page.js
+++ src/app/systems/page.js
@@ -3,21 +3,63 @@
 import SystemItem from "@/components/SystemItem";
 import { useAccount } from "@/context/AccountContext";
 import { getSystems } from "@/services/api";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+
+const TOTAL = 12000;
+const PER_PAGE = 50;
+const MAX_PAGES = TOTAL / PER_PAGE;
 
 export default function Systems() {
   const [systems, setSystems] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [controlledSystem, setControlledSystem] = useState("");
+  const [controlledPage, setControlledPage] = useState(1);
+  const router = useRouter();
   const { account } = useAccount();
 
   useEffect(() => {
-    async function fetching() {
-      const data = await getSystems(account.token);
-      setSystems(data);
+    async function fetching(page) {
+      const arr = [];
+      for (let i = (page - 1) * 5 + 1; i <= page * 5; i++) {
+        const {data} = await getSystems(account.token, i);
+        arr.push(data);
+      }
+      setSystems(arr.flat());
     }
-    fetching();
-  }, [account.token]);
+    fetching(currentPage);
+  }, [account.token, currentPage]);
 
   return <section>
+    <div className="uppercase flex gap-4 mb-2">
+        <span>page: {currentPage}</span>
+        <span>pages: {MAX_PAGES}</span>
+        <span>on page: {PER_PAGE}</span>
+        <span>total systems: {TOTAL}</span>
+        <form onSubmit={(e) => {
+          e.preventDefault();
+          setCurrentPage(controlledPage);
+        }}>
+          <label>
+            <span>Go to: </span>
+            <input value={controlledPage} onChange={(e) => {
+              if (e.target.value <= 1) return setControlledPage(1);
+              if (e.target.value >= MAX_PAGES) return setControlledPage(MAX_PAGES);
+              if (!isNaN(+e.target.value)) setControlledPage(+e.target.value);
+            }} className="bg-transparent border-stone-700 border rounded-md px-3 w-16 text-center"/>
+          </label>
+        </form>
+        <form className="ml-auto" onSubmit={(e) => {
+          e.preventDefault();
+          router.push(`systems/${controlledSystem}`);
+        }}>
+          <label>
+            <span>Find a system: </span>
+            <input onChange={(e) => setControlledSystem(e.target.value)} value={controlledSystem} className="bg-transparent border-stone-700 border rounded-md px-3 w-28 text-center"/>
+          </label>
+        </form>
+    </div>
+
     <ul className="space-y-5">
       {systems?.map((data) => <SystemItem key={data.symbol} system={data}/>)}
     </ul>
