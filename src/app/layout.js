7eda787726ff2e2a4fd034372957557d51df5bfc
Stormblessed <carlomanid@gmail.com>
2023-09-17 23:22:12 +0300

    Add ShipsProvider

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import './globals.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AccountProvider from '@/context/AccountContext';
 import Footer from '@/components/Footer';
+import ShipsProvider from '@/context/ShipsContext';
 
 export const metadata = {
   title: 'Create Next App',
@@ -24,13 +25,15 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`font-speedy relative text-stone-50 bg-stone-950`}>
         <AccountProvider>
-          <QueryClientProvider client={queryClient}>
-            <Header/>
-            <main className="max-w-7xl mx-auto min-h-[80vh]">
-              {children}
-            </main>
-            <Footer/>
-          </QueryClientProvider>
+            <QueryClientProvider client={queryClient}>
+              <Header/>
+              <main className="max-w-7xl mx-auto min-h-[80vh]">
+                <ShipsProvider>
+                {children}
+                </ShipsProvider>
+              </main>
+              <Footer/>
+            </QueryClientProvider>
         </AccountProvider>
       </body>
     </html>
