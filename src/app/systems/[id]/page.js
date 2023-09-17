db5db72f300db395af667bf57fcce89ebd8a15ad
Stormblessed <carlomanid@gmail.com>
2023-09-17 23:27:23 +0300

    Add icons

--- src/app/systems/[id]/page.js
+++ src/app/systems/[id]/page.js
@@ -2,14 +2,15 @@
 
 import Planets from "@/components/Planets";
 import WaypointItem from "@/components/WaypointItem";
+import { useShips } from "@/context/ShipsContext";
 import { useAccount } from "@/context/AccountContext";
 import { getShips, getWaypoints } from "@/services/api";
 import { useEffect, useState } from "react";
 
 export default function System({ params }) {
-  const { account, rerender } = useAccount();
+  const { account } = useAccount();
+  const { ships, rerender, setShips } = useShips();
   const [system, setSystem] = useState([]);
-  const [ships, setShips] = useState([]);
 
   const uniqueWaypoints = [...new Set(ships.map(ship => ship.nav.status === "IN_TRANSIT" ? "" : ship.nav.waypointSymbol))].filter((item) => item.includes(params.id));
 
@@ -41,19 +42,19 @@ export default function System({ params }) {
       setSystem(data);
     }
     fetching();
-  }, [params.id, account.token, account.credits, rerender]);
+  }, [params.id, account.token, account.credits, rerender, setShips]);
   
   return <section className="space-y-10">
-    <Planets waypoints={system}/>
+      <Planets className="px-0" icons={true} waypoints={system}/>
 
-    <h2 className="text-6xl font-bold">{params.id}</h2>
+      <h2 className="text-6xl font-bold">{params.id}</h2>
 
-    <ul className="grid grid-cols-3 gap-6">
-      {transit.map(({waypoint, ships}) => <WaypointItem system={system} params={params} ships={ships} waypoint={waypoint} key={waypoint.type}/>)}
-    </ul>
-    {!collectionOfShips.length && <p className="text-center text-2xl">No ships in orbit or docked</p>}
-    <ul className="grid grid-cols-2 gap-6">
-      {collectionOfShips.map(({waypoint, ships}) => <WaypointItem system={system} params={params} ships={ships} waypoint={waypoint} key={waypoint.name}/>)}
-    </ul>
+      <ul className="grid grid-cols-3 gap-6">
+        {transit.map(({waypoint, ships}) => <WaypointItem system={system} params={params} ships={ships} waypoint={waypoint} key={waypoint.type}/>)}
+      </ul>
+      {!collectionOfShips.length && <p className="text-center text-2xl">No ships in orbit or docked</p>}
+      <ul className="grid grid-cols-2 gap-6">
+        {collectionOfShips.map(({waypoint, ships}) => <WaypointItem system={system} params={params} ships={ships} waypoint={waypoint} key={waypoint.name}/>)}
+      </ul>
   </section>
 }
--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,23 +1,25 @@
 import PlanetItem from "./PlanetItem";
 
-export default function Planets({ waypoints = [] }) {
-    return <ul className="flex gap-4 justify-around flex-wrap px-5 pb-5 text-center">
+import { GiShipWheel, GiTakeMyMoney } from "react-icons/gi";
+
+export default function Planets({ waypoints = [], className = "", icons = false }) {
+    return <ul className={`${className} flex gap-4 justify-around flex-wrap px-5 pb-5 text-center `}>
         {waypoints.map((point) => {
 
             const hasShipyard = point.traits?.find(item => item.symbol === "SHIPYARD");
             const hasMarketplace = point.traits?.find(item => item.symbol === "MARKETPLACE");
 
-            return <li className="flex flex-col justify-start" key={point.symbol}>
+            return <li className={`flex flex-col justify-start`} key={point.symbol}>
                 <PlanetItem type={point.type}/>
                 <span className="text-[0.6rem]">{point.symbol}</span>
-                <div className="flex flex-col justify-center gap-2 mt-2">
-                    {!!hasShipyard && <div className="text-xs">
-                        shipyard
-                    </div>}
-                    {!!hasMarketplace && <div className="text-xs">
-                        market
-                    </div>}
-                </div>
+                {icons && <div className="grid grid-cols-2 gap-2 mt-2">
+                    {!!hasShipyard ?
+                    <GiShipWheel className="bg-amber-600 p-2 rounded-full w-10 h-10 border border-stone-50 justify-self-end"/> :
+                    <GiShipWheel className="bg-stone-700 p-2 rounded-full w-10 h-10 border border-stone-50 justify-self-end"/>}
+                    {!!hasMarketplace ?
+                    <GiTakeMyMoney className="bg-amber-600 p-2 rounded-full w-10 h-10 border border-stone-50 justify-self-start"/> :
+                    <GiTakeMyMoney className="bg-stone-700 p-2 rounded-full w-10 h-10 border border-stone-50 justify-self-start"/>}
+                </div>}
             </li>
         })}
     </ul>
