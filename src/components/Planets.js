8f724a3c1293f1aed98bd7f9d261fdd11ca2c9a5
Stormblessed <carlomanid@gmail.com>
2023-09-19 23:03:37 +0300

    Fix styles

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,18 +1,19 @@
-import PlanetItem from "./PlanetItem";
+import PlanetImg from "./PlanetImg";
 
 import { GiShipWheel, GiTakeMyMoney } from "react-icons/gi";
 
 export default function Planets({ waypoints = [], className = "", icons = false }) {
-    return <ul className={`${className} flex gap-4 justify-around flex-wrap px-5 pb-5 text-center `}>
+    return <ul className={`${className} flex gap-4 justify-around items-end flex-wrap px-5 pb-5 text-center `}>
         {waypoints.map((point) => {
 
             const hasShipyard = point.traits?.find(item => item.symbol === "SHIPYARD");
             const hasMarketplace = point.traits?.find(item => item.symbol === "MARKETPLACE");
 
-            return <li className={`flex flex-col justify-start`} key={point.symbol}>
-                <PlanetItem type={point.type}/>
-                <span className="text-[0.6rem]">{point.symbol}</span>
-                {icons && <div className="grid grid-cols-2 gap-2 mt-2">
+            return <li className={`flex flex-col justify-end items-center mt-6`} key={point.symbol}>
+                <PlanetImg type={point.type}/>
+                <span className="text-[0.6rem] mt-4">{point.symbol}</span>
+
+                {icons && <div className="grid grid-cols-2 gap-2 mt-4">
                     {!!hasShipyard ?
                     <GiShipWheel className="bg-amber-600 p-2 rounded-full w-10 h-10 border border-stone-50 justify-self-end"/> :
                     <GiShipWheel className="bg-stone-700 p-2 rounded-full w-10 h-10 border border-stone-50 justify-self-end"/>}
