d0dfc6b05204dd3f70bbbf6fd998e9448b1880b0
Stormblessed <carlomanid@gmail.com>
2023-09-19 21:26:25 +0300

    Add a portal and derive the faction window

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,11 @@
 import { useAccount } from "@/context/AccountContext";
 import { getAgent, getFactions, signUp } from "@/services/api";
 import { useEffect, useState } from "react";
-import { AiOutlineInfoCircle, AiOutlineClose } from "react-icons/ai";
+import { createPortal } from "react-dom";
+import { AiOutlineInfoCircle } from "react-icons/ai";
+import { FaTrashAlt } from "react-icons/fa";
+import FactionInfo from "./FactionInfo";
+
 
 export default function Login({ onClose }) {
     const oldStorage = JSON.parse(localStorage.getItem("agent")) ?? [];
@@ -24,7 +28,6 @@ export default function Login({ onClose }) {
     const create = currentTab === "create";
     const activeClass = "text-amber-600";
 
-
     async function handleSignUp(e) {
         e.preventDefault();
         const data = await signUp(username, selectedFaction);
@@ -32,30 +35,38 @@ export default function Login({ onClose }) {
         const acc = {token: data.token, name: data.agent.symbol, credits: data.agent.credits};
         setAccount(acc);
 
-        if (oldStorage?.find((item) => item.name === data.agent.symbol)) return;
+        if (oldStorage?.find((item) => item.name === data.agent.symbol)) return onClose(false);
 
-        await localStorage.setItem("agent", JSON.stringify([acc, ...oldStorage]));
-        setLocal(JSON.parse(localStorage.getItem("agent")));
+        const newStorage = [acc, ...oldStorage];
+        localStorage.setItem("agent", JSON.stringify(newStorage));
+        setLocal(newStorage);
         onClose(false);
     }
 
-    async function handleAuth(e) {
+    async function handleAuth(e, token) {
         e.preventDefault();
         const data = await getAgent(token, "agent");
-
         const acc = {token, name: data.symbol, credits: data.credits};
         setAccount(acc);
 
-        if (oldStorage?.find((item) => item.name === data.symbol)) return;
+        if (oldStorage?.find((item) => item.name === data.symbol)) return onClose(false);
 
-        await localStorage.setItem("agent", JSON.stringify([acc, ...oldStorage]));
-        setLocal(JSON.parse(localStorage.getItem("agent")));
+        const newStorage = [acc, ...oldStorage];
+        localStorage.setItem("agent", JSON.stringify(newStorage));
+        setLocal(newStorage);
         onClose(false);
     }
 
     function handleSwitchAcc(data) {
         setAccount(data);
         onClose(false);
+    }
+
+    function handleRemove(e, token) {
+        e.stopPropagation();
+        const removed = oldStorage.filter((item) => item.token !== token);
+        localStorage.setItem("agent", JSON.stringify(removed));
+        setLocal(removed);
     }
 
     useEffect(() => {
@@ -66,9 +77,9 @@ export default function Login({ onClose }) {
         fetching();
     }, [token])
 
-    return <div className="fixed z-[1000] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-stone-900 flex flex-col w-[30rem]">
+    return <div className="window w-[30rem]">
         <div className="flex">
-            <button onClick={() => setCurrentTav("switch")} className={`relative py-4 grow uppercase ${switchAcc ? activeClass : ""} hover`}>
+            <button onClick={() => setCurrentTav("switch")} className={`relative py-4 grow uppercase ${switchAcc ? activeClass : ""}`}>
                 Switch
                 {switchAcc &&  <span className="absolute left-0 bottom-0 bg-amber-600 h-0.5 w-full"></span>}
             </button>
@@ -82,64 +93,35 @@ export default function Login({ onClose }) {
             </button>
         </div>
 
-        {switchAcc && <ul className="cursor-pointer">
-            {local.map((data) => <li className="p-5 hover:bg-stone-700" key={data.token} onClick={() => handleSwitchAcc(data)}>{data.name}</li>)}
-        </ul>
-        }
+        {switchAcc && <ul className="max-h-[70vh] overflow-auto">
+            {local.length ? local.map((agent) => <li className="cursor-pointer p-5 rounded-md hover:bg-stone-700 flex justify-between items-center" key={agent.token} onClick={(e) => handleAuth(e, agent.token)}>
+                <span>{agent.name}</span>
+                <button onClick={(e) => handleRemove(e, agent.token)}>
+                    <FaTrashAlt className="hover:fill-amber-600 h-6 w-6"/>
+                </button>
+            </li>) : <li className="p-5 flex justify-center items-center">NO ACCOUNTS</li>}
+        </ul>}
 
-        {login && <form onSubmit={handleAuth} className="flex flex-col gap-8 p-5">
-            <input value={token} onChange={(e) => setToken(e.target.value)} placeholder="Token" className="bg-transparent border-stone-700 border rounded-md px-3 py-3"/>
+        {login && <form onSubmit={(e) => handleAuth(e, token)} className="flex flex-col gap-8 p-5">
+            <input value={token} onChange={(e) => setToken(e.target.value)} placeholder="Token" className="input py-3"/>
             <button className="btn-color hover:btn-color-hover text-xl">Set username</button>
         </form>}
 
         {create && <form  onSubmit={handleSignUp} className="flex flex-col gap-8 p-5">
-            <input value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" className="bg-transparent border-stone-700 border rounded-md px-3 py-3"/>
+            <input value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" className="input py-3"/>
 
             <div className="flex justify-between gap-2">
-                <select className="bg-transparent border-stone-700 border rounded-md px-3 py-3" onChange={(e) => setSelectedFaction(e.target.value)} value={selectedFaction}>
+                <select className="input py-3 grow" onChange={(e) => setSelectedFaction(e.target.value)} value={selectedFaction}>
                     {factions.map(faction => <option className="bg-stone-900" value={faction.symbol} key={faction.symbol}>{faction.name}</option>)}
                 </select>
                 <button onClick={() => setIsInfoOpen(true)} type="button">
-                    <AiOutlineInfoCircle className="h-8 w-8"/>
+                    <AiOutlineInfoCircle className="h-8 w-8 rounded-full hover:fill-amber-600"/>
                 </button>
             </div>
             
             <button className="btn-color hover:btn-color-hover text-xl">Set username</button>
         </form>}
 
-        {isInfoOpen && <div className="fixed z-[1001] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-stone-900 flex flex-col w-[40rem] border-solid border border-amber-600 p-4 space-y-4">
-            <button onClick={() => setIsInfoOpen(false)} className="absolute z-[1002] top-0 right-0 translate-x-1/2 -translate-y-1/2 rounded-[50%] border-solid border border-amber-600 p-2 text-sm] bg-stone-900 hover:bg-amber-600"><AiOutlineClose/></button>
-            <div>
-                <div className="flex items-center gap-2 mb-2">
-                    <span className="block text-xs text-stone-500 uppercase">NAME</span>
-                    <hr className="opacity-50 grow"/>
-                </div>
-                <p>{selectedFactionInfo.name}</p>
-            </div>
-            <div>
-                <div className="flex items-center gap-2 mb-2">
-                    <span className="block text-xs text-stone-500 uppercase">DESCRIPTION</span>
-                    <hr className="opacity-50 grow"/>
-                </div>
-                <p>{selectedFactionInfo.description}</p>
-            </div>
-            <div>
-                <div className="flex items-center gap-2 mb-2">
-                    <span className="block text-xs text-stone-500 uppercase">HEADQUARTERS</span>
-                    <hr className="opacity-50 grow"/>
-                </div>
-                <p>{selectedFactionInfo.headquarters}</p>
-            </div>
-            <div>
-                <div className="flex items-center gap-2 mb-2">
-                    <span className="block text-xs text-stone-500 uppercase">TRAITS</span>
-                    <hr className="opacity-50 grow"/>
-                </div>
-                <ul className="flex gap-4">
-                    {selectedFactionInfo.traits.map((trait) => <li className="rounded-full bg-stone-700 text-sm p-2 " key={trait.symbol}>{trait.name}</li>)}
-                </ul>
-            </div>
-        </div>}
-
+        {isInfoOpen && createPortal(<FactionInfo selectedFactionInfo={selectedFactionInfo} onClose={setIsInfoOpen}/>, document.body)}
     </div>
 }
