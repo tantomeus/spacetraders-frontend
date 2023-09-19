17a344b7a4e4acbaa5d686af379a51f2aaf87a4c
Stormblessed <carlomanid@gmail.com>
2023-09-19 21:27:51 +0300

    Create ships page

---
+++ src/app/ships/page.js
@@ -0,0 +1,51 @@
+"use client";
+
+import PlanetItem from "@/components/PlanetImg";
+import ShipItem from "@/components/ShipItem";
+import { useAccount } from "@/context/AccountContext";
+import { useShips } from "@/context/ShipsContext";
+import { getShips } from "@/services/api";
+import { useEffect } from "react";
+
+export default function Ships() {
+    const { ships, setShips } = useShips();
+    const { account } = useAccount();
+
+    const uniqueSystems = [...new Set(ships.filter(ship => ship.nav.status !== "IN_TRANSIT").map(ship => ship.nav.systemSymbol))];
+
+    const collectionOfShips = uniqueSystems.map(system => ({
+        system,
+        ships: ships.filter(ship => ship.nav.systemSymbol === system)
+    }));
+
+    const transit = {
+        ships: ships.filter(ship => ship.nav.status === "IN_TRANSIT")
+    }
+
+    useEffect(() => {
+        async function fetching() {
+            const data = await getShips(account.token);
+            setShips(data);
+        }
+        fetching();
+    }, [account.token, setShips]);
+
+    return <section>
+        <ul className="space-y-5">
+            {collectionOfShips.map(({system, ships}) => {
+                return <li key={system} className="bg-stone-900 rounded-md">
+                <div className="p-4">
+                    <div className="min-h-[4.28rem] flex justify-between items-center">
+                        <h2 className="text-2xl font-bold uppercase">{system}</h2>
+                        <PlanetItem/>
+                    </div>
+                </div>
+                <hr/>
+                <ul className="space-y-4">
+                {ships.map(ship => <ShipItem key={ship.symbol} system={ship.system} ship={ship}/>)}
+                </ul>
+            </li>
+            })}
+        </ul>
+    </section>
+}
