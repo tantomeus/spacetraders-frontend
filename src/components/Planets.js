aca36d8cc8aed48ee1f469ff9a1a7ba1cd0ff549
Stormblessed <carlomanid@gmail.com>
2023-09-22 22:36:52 +0300

    Refactoring

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,9 +1,9 @@
 import PlanetImg from "./PlanetImg";
-
 import { GiShipWheel, GiTakeMyMoney } from "react-icons/gi";
 
 export default function Planets({ waypoints = [], className = "", icons = false }) {
-    return <ul className={`${className} flex gap-4 justify-around items-end flex-wrap px-5 pb-5 text-center `}>
+    return (
+    <ul className={`${className} flex gap-4 justify-around items-end flex-wrap px-5 pb-5 text-center`}>
         {waypoints.map((point) => {
 
             const hasShipyard = point.traits?.find(item => item.symbol === "SHIPYARD");
@@ -13,15 +13,19 @@ export default function Planets({ waypoints = [], className = "", icons = false 
                 <PlanetImg type={point.type}/>
                 <span className="text-[0.6rem] mt-4">{point.symbol}</span>
 
-                {icons && <div className="grid grid-cols-2 gap-2 mt-4">
-                    {!!hasShipyard ?
-                    <GiShipWheel className="bg-amber-600 p-2 rounded-full w-10 h-10 border border-stone-50 justify-self-end"/> :
-                    <GiShipWheel className="bg-stone-700 p-2 rounded-full w-10 h-10 border border-stone-50 justify-self-end"/>}
-                    {!!hasMarketplace ?
-                    <GiTakeMyMoney className="bg-amber-600 p-2 rounded-full w-10 h-10 border border-stone-50 justify-self-start"/> :
-                    <GiTakeMyMoney className="bg-stone-700 p-2 rounded-full w-10 h-10 border border-stone-50 justify-self-start"/>}
+                {icons&& <div className="grid grid-cols-2 gap-2 mt-4">
+                    {!!hasShipyard 
+                    ? <GiShipWheel
+                    className="bg-amber-600 p-2 rounded-full icon-size-primary white-border justify-self-end"/>
+                    : <GiShipWheel
+                    className="disable-color p-2 rounded-full icon-size-primary white-border justify-self-end"/>}
+                    {!!hasMarketplace
+                    ? <GiTakeMyMoney
+                    className="bg-amber-600 p-2 rounded-full icon-size-primary white-border justify-self-start"/>
+                    : <GiTakeMyMoney
+                    className="disable-color p-2 rounded-full icon-size-primary white-border justify-self-start"/>}
                 </div>}
             </li>
         })}
-    </ul>
+    </ul>)
 }
