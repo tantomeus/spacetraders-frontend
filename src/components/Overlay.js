57680b285bd0ffcaea20400e5ad3f3fcb22716ba
Stormblessed <carlomanid@gmail.com>
2023-09-22 22:36:10 +0300

    Minor fix

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -1,3 +1,5 @@
 export default function Overlay({ onClose }) {
-    return <div onClick={() => onClose(false)} className="z-[500] absolute inset-0 opacity-50 bg-stone-950"></div>
+    return <div onClick={() => onClose("")}
+    className="z-[500] absolute inset-0 opacity-50 bg-stone-950">
+    </div>
 }
