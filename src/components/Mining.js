150636574f03d2f5602eacc51c19a6ea56b3af73
Stormblessed <carlomanid@gmail.com>
2023-09-22 22:40:50 +0300

    Derive ships update to parent

--- src/components/Mining.js
+++ src/components/Mining.js
@@ -1,41 +1,46 @@
 "use client";
 
+import { useAccount } from "@/context/AccountContext";
 import { convertSeconds } from "@/helpers/helpers";
 import { mineAsteroid } from "@/services/api";
-import { useEffect } from "react";
 
-export default function Mining({ ship, account, waypoint, setShips }) {
-    const { remainingSeconds } = ship.cooldown;
+export default function Mining({ ship, waypoint }) {
+    const { account, setShips } = useAccount();
     const resources = waypoint.traits?.filter(({ symbol }) => symbol.includes("DEPOSITS"));
     const isShipDocked = ship.nav.status !=="IN_ORBIT";
 
+    const { remainingSeconds } = ship.cooldown;
+
     async function handleMineAsteroid() {
-        const data = await mineAsteroid(account.token, ship.symbol);
-        setShips((ships) => ships.map((shipState) => ship.symbol === shipState.symbol ? {...shipState, cargo: data.cargo, cooldown: data.cooldown} : shipState));
+        try {
+            const data = await mineAsteroid(account.token, ship.symbol);
+
+            if (!data) throw new Erorr("erorp");
+            
+            setShips((ships) => ships.map((shipState) => ship.symbol === shipState.symbol ?
+            {...shipState, cargo: data.cargo, cooldown: data.cooldown} :
+            shipState));
+        } catch(err) {
+            console.error(err);
+        }
     }
 
-    useEffect(() => {
-        if (remainingSeconds) {
-            const interval = setInterval(() => {
-                setShips((ships) => ships.map((shipState) => ship.symbol === shipState.symbol ? {...shipState, cooldown: {...shipState.cooldown, remainingSeconds: shipState.cooldown.remainingSeconds - 1}} : shipState));
-            }, 1000);
-    
-            return function() {
-                clearInterval(interval);
-            }
-        }
-    }, [remainingSeconds, ship.symbol, setShips]);
-
-    return <div className="window window-divide w-[40rem]">
-        <div className="flex items-center justify-between p-4">
+    return (
+    <div className="window window-divide w-[40rem]">
+        <div className="flex-between p-4">
             <h2 className="text-2xl">Mining</h2>
             {!!remainingSeconds && <span>{convertSeconds(remainingSeconds)}</span>}
-            {!remainingSeconds && <button disabled={isShipDocked} onClick={handleMineAsteroid} className={isShipDocked ? "bg-stone-700 btn-color" : "btn-color hover:btn-color-hover"}>MINE!</button>}
+            {!remainingSeconds && <button disabled={isShipDocked} onClick={handleMineAsteroid}
+            className={`btn ${isShipDocked
+            ? "disable-color"
+            : "btn-color hover:btn-color-reversed"}`}>
+                MINE!
+            </button>}
         </div>
 
         <div className="p-4 space-y-4">
-            <p className="text-xl">Possible resources</p>
-            {resources?.map(resource => <div key={resource.symbol}>
+            {resources?.map(resource => 
+            <div key={resource.symbol}>
                 <div className="flex gap-2 items-center">
                     <h3 className="text-lg">{resource.name.replace(" Deposits", "")}</h3>
                     <hr className="opacity-50 grow"/>
@@ -43,5 +48,5 @@ export default function Mining({ ship, account, waypoint, setShips }) {
                 <p>{resource.description}</p>
             </div>)}
         </div>
-    </div>
+    </div>)
 }
