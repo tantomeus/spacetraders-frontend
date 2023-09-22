7af64e8a586d9733554df2e42682a1a64b389f66
Stormblessed <carlomanid@gmail.com>
2023-09-22 22:35:28 +0300

    Refactoring

--- src/components/FlightWindow.js
+++ src/components/FlightWindow.js
@@ -1,16 +1,23 @@
 import { convertSeconds } from "@/helpers/helpers";
 
-export default function FlightWindow({ ship, timer }) {
+export default function FlightWindow({ ship, seconds }) {
 
-    return <div className="fixed z-[1000] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-stone-900 flex flex-col w-96 divide-y divide-stone-500">
-        <div className="flex p-4 justify-between items-center">
+    return (
+    <div className="window window-divide w-96">
+        <div className="p-4 flex-between">
             <h2 className="text-2xl">Arrives</h2>
-            <span>{convertSeconds(timer)}</span>
+            <span>{convertSeconds(seconds)}</span>
         </div>
+
         <div className="grid grid-cols-2 gap-4 p-4 text-sm">
-            <span>Fuel Consumed</span> <span className="justify-self-end text-stone-400">{ship.fuel.consumed.amount}</span>
-            <span>Destination</span> <span className="justify-self-end text-stone-400">{ship.nav.route.destination.symbol}</span>
-            <span>Mode</span> <span className="justify-self-end text-stone-400">{ship.nav.flightMode}</span>
+            <span>Fuel Consumed</span>
+            <span className="justify-self-end text-stone-400">{ship.fuel.consumed.amount}</span>
+            <span>Departure</span>
+            <span className="justify-self-end text-stone-400">{ship.nav.route.departure.symbol}</span>
+            <span>Destination</span>
+            <span className="justify-self-end text-stone-400">{ship.nav.route.destination.symbol}</span>
+            <span>Mode</span>
+            <span className="justify-self-end text-stone-400">{ship.nav.flightMode}</span>
         </div>
-    </div>
+    </div>)
 }
--- src/components/TravelWindow.js
+++ src/components/TravelWindow.js
@@ -1,17 +1,17 @@
 "use client";
 
 import { useAccount } from "@/context/AccountContext";
-import { useShips } from "@/context/ShipsContext";
 import { flyToWaypoint, switchFlightMode, warpOrJump } from "@/services/api"
 import { useState } from "react";
 import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 
-export default function TravelWindow({ waypoints, departureSymbol, ship, onNavigation }) {
-    const { account } = useAccount();
-    const { setRerender, setShips } = useShips();
+export default function TravelWindow({ waypoints, ship, onNavigation }) {
+    const { account, setShips, fetchShipsData } = useAccount();
     const [controlledWaypoint, setControlledWaypoint] = useState("");
     const [translateX, setTranslateX] = useState("0%");
-    
+
+    const isShipDocked = ship.nav.status === "DOCKED";
+    const departureSymbol = ship.nav.waypointSymbol;
     const flightMode = ship.nav.flightMode;
 
     const transformStyles = {
@@ -28,83 +28,110 @@ export default function TravelWindow({ waypoints, departureSymbol, ship, onNavig
         return {fuel: 1, speed: 1}
     }
 
-    function handleFlightMode(token, ship, mode) {
-        switchFlightMode(token, ship, mode);
-        setShips((ships) => ships.map((stateShip) => {
-            const newShip = stateShip;
-            if (newShip.symbol !== ship) return newShip;
-            newShip.nav.flightMode = mode;
-            return newShip;
-        }));
-    }
+    async function handleFlightMode(token, ship, mode) {
+        try {
+            const data = await switchFlightMode(token, ship, mode);
 
-    function handleTravel(e, token, ship, destination, type) {
-        if (type) {
-            e.preventDefault();
-            warpOrJump(token, ship, destination, type);
+            if (!data) throw new Error("error!");
+
+            setShips((ships) => ships.map((item) => item.symbol === ship ? {...item, nav: data.nav} : item));
+        } catch(err) {
+            console.error(err);
         }
-        else flyToWaypoint(token, ship, destination);
-
-        onNavigation(false);
-        setRerender((rerender) => !rerender);
     }
 
-    return <div className="window window-divide w-[38rem]">
-        <div className="flex justify-between items-center px-6 py-4">
+    async function handleTravel(e, token, ship, destination, type) {
+        e.preventDefault();
+        try {
+            let data;
+            if (type) data = await warpOrJump(token, ship, destination, type);
+            else data = await flyToWaypoint(token, ship, destination);
+
+            if (!data) throw new Error("another error");
+            
+            onNavigation(false);
+            fetchShipsData(token);
+        } catch(err) {
+            console.error(err);
+        }
+    }
+
+    return (
+    <div className="window w-[38rem]">
+        <div className="flex-between px-6 py-4">
             <h2 className="text-2xl">Travel</h2>
+
             <div className="flex justify-center items-center">
                 <button onClick={() => handleFlightMode(account.token, ship.symbol, "CRUISE")}
-                className={`${flightMode === "CRUISE" ? "bg-amber-600" : "hover:bg-stone-50 hover:text-amber-600"} border p-2 text-xs/[1rem] skew-x-[20deg]`}>
+                className={`${flightMode === "CRUISE" ? "btn-color" : "hover:btn-color-reversed"} border p-2 text-xs/[1rem] skew-x-[20deg]`}>
                     <span className="block skew-x-[-20deg]">CRUISE</span>
                 </button>
+
                 <button onClick={() => handleFlightMode(account.token, ship.symbol, "BURN")}
-                className={`${flightMode === "BURN" ? "bg-amber-600" : "hover:bg-stone-50 hover:text-amber-600"} border p-2 text-xs/[1rem] skew-x-[20deg]`}>
+                className={`${flightMode === "BURN" ? "btn-color" : "hover:btn-color-reversed"} border p-2 text-xs/[1rem] skew-x-[20deg]`}>
                     <span className="block skew-x-[-20deg]">BURN</span>
                 </button>
+
                 <button onClick={() => handleFlightMode(account.token, ship.symbol, "DRIFT")}
-                className={`${flightMode === "DRIFT" ? "bg-amber-600" : "hover:bg-stone-50 hover:text-amber-600"} border p-2 text-xs/[1rem] skew-x-[20deg]`}>
+                className={`${flightMode === "DRIFT" ? "btn-color" : "hover:btn-color-reversed"} border p-2 text-xs/[1rem] skew-x-[20deg]`}>
                     <span className="block skew-x-[-20deg]">DRIFT</span>
                 </button>
+                
                 <button onClick={() => handleFlightMode(account.token, ship.symbol, "STEALTH")}
-                className={`${flightMode === "STEALTH" ? "bg-amber-600" : "hover:bg-stone-50 hover:text-amber-600"} border p-2 text-xs/[1rem] skew-x-[20deg]`}>
+                className={`${flightMode === "STEALTH" ? "btn-color" : "hover:btn-color-reversed"} border p-2 text-xs/[1rem] skew-x-[20deg]`}>
                     <span className="block skew-x-[-20deg]">STEALTH</span>
                 </button>
             </div>
         </div>
 
-        <ul className="overflow-y-auto max-w-full max-h-[70vh] pt-4 text-xs overflow-x-hidden">
+        <ul className="h-overflow max-w-full pt-4 text-xs">
             <li style={transformStyles} className="relative flex w-[200%]">
-
+                
                 <AiOutlineLeft
-                onClick={() => setTranslateX("-50%")}
+                onClick={() => {
+                    setTranslateX("-50%");
+                    setControlledWaypoint("");
+                }}
                 className="cursor-pointer hover:fill-amber-500 absolute h-4 w-4 top-[50%] right-[50%] -translate-y-1/2 -translate-x-1/2"/>
+
                 <AiOutlineRight
-                onClick={() => setTranslateX("0%")}
+                onClick={() => {
+                    setTranslateX("0%");
+                    setControlledWaypoint("");
+                }}
                 className="cursor-pointer hover:fill-amber-500 absolute h-4 w-4 top-[50%] right-[0] -translate-y-1/2 -translate-x-1/2"/>
 
                 {["warp", "jump"].map(module => {
                 const hasModule = !ship.modules.find(({symbol}) => symbol.includes(module.toUpperCase()));
 
-                if (hasModule) return <p key={module} className="pl-6 pr-8 py-3 w-[50%] text-center text-xl">The ship does not have a {module} module</p>
+                    if (hasModule) return <p key={module}
+                    className="pl-6 pr-8 py-3 w-[50%] text-center text-xl">
+                        The ship does not have a {module} module
+                    </p>
 
-                return <form key={module}
-                className="pl-6 pr-8 py-3 grid grid-cols-[2fr_1fr_2fr_0.5fr_5.4rem] items-center w-[50%]"
-                onSubmit={(e) => handleTravel(e, account.token, ship.symbol, controlledWaypoint, module)}>
-                    <input placeholder="WAYPOINT"
-                    onChange={(e) => setControlledWaypoint(e.target.value)} value={controlledWaypoint}
-                    className="input h-full"/>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <button
-                    className=" btn-color hover:btn-color-hover text-xs/[1rem]">{module}</button>
-                </form>
+                    return <form key={module}
+                    className="pl-6 pr-8 py-3 grid grid-cols-[2fr_1fr_2fr_0.5fr_5.4rem] items-center w-[50%]"
+                    onSubmit={(e) => handleTravel(e, account.token, ship.symbol, controlledWaypoint, module)}>
+                        <input placeholder="WAYPOINT"
+                        onChange={(e) => setControlledWaypoint(e.target.value)} value={controlledWaypoint}
+                        className="input h-full"/>
+
+                        <span></span>
+                        <span></span>
+                        <span></span>
+
+                        <button
+                        onClick={(e) => handleTravel(e, account.token, ship.symbol, planet.symbol)}
+                        disabled={isShipDocked}
+                        className={`btn ${isShipDocked ? "disable-color" : "btn-color hover:btn-color-reversed"} text-xs/[1rem]`}>
+                            {module}
+                        </button>
+                    </form>
                 })}
             </li>
 
             {waypoints.map((planet, i, arr) => {
 
-            const isShipDocked = ship.nav.status === "DOCKED";
             const isCurrentWaypount = planet.symbol === departureSymbol;
             const canFly = !isCurrentWaypount && !isShipDocked;
 
@@ -114,8 +141,11 @@ export default function TravelWindow({ waypoints, departureSymbol, ship, onNavig
 
             const displayed = (timeSpending || 10) * 3 * flightModeFactor(flightMode).speed;
 
-            return <li className="py-3 pl-6 pr-8 grid grid-cols-[2fr_1fr_2fr_0.5fr_5.4rem] items-center" key={planet.symbol}>
+            return (
+            <li className="py-3 pl-6 pr-8 grid grid-cols-[2fr_1fr_2fr_0.5fr_5.4rem] items-center"
+            key={planet.symbol}>
                 <span>{planet.symbol}</span>
+
                 <span></span>
                 <div className="flex flex-col">
                     {!isCurrentWaypount && displayed >= 60 && <span>~{Math.ceil(displayed/60)} Minutes</span>}
@@ -124,8 +154,14 @@ export default function TravelWindow({ waypoints, departureSymbol, ship, onNavig
                     {!isCurrentWaypount && <span>~{fuelSpending} Fuel</span>}
                 </div>
                 <span></span>
-                <button onClick={(e) => handleTravel(e, account.token, ship.symbol, planet.symbol)} disabled={!canFly} className={`btn-color ${!canFly ? "bg-stone-600" : "hover:btn-color-hover"} text-xs/[1rem]`}>SELECT</button>
-            </li>})}
+
+                <button
+                onClick={(e) => handleTravel(e, account.token, ship.symbol, planet.symbol)}
+                disabled={!canFly}
+                className={`btn ${!canFly ? "disable-color" : "btn-color hover:btn-color-reversed"} text-xs/[1rem]`}>
+                    SELECT
+                </button>
+            </li>)})}
         </ul>
-    </div>
+    </div>)
 }
