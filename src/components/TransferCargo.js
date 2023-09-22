2b4b40fd060e69d69de1f2f9a022f133a48d2175
Stormblessed <carlomanid@gmail.com>
2023-09-22 22:38:00 +0300

    Add jsx markup

--- src/components/TransferCargo.js
+++ src/components/TransferCargo.js
@@ -1,20 +1,44 @@
 "use client";
 
 import { useState } from "react";
-import ShipImg from "./ShipImg";
+import { useAccount } from "@/context/AccountContext";
+
 import TransferCargoItem from "./TransferCargoItem";
+import SelectShip from "./SelectShip";
+import BackButton from "./BackButton";
 
-export default function TransferCargo({ ship, secondShip }) {
+export default function TransferCargo({ ship }) {
+    const { ships } = useAccount();
+    const [selectedShip, setSelectedShip] = useState({});
+    const availableShips = ships.filter(item => ship.symbol !== item.symbol
+        && ship.nav.waypointSymbol === item.nav.waypointSymbol
+        && ship.nav.status === item.nav.status);
 
-    return <div className="window window-divide w-[40rem]">
-        <div className="flex items-center justify-between p-4">
+    return (
+    <div className="window window-divide w-[50rem]">
+        <div className="flex-between p-4">
             <h2 className="text-2xl">Transfer</h2>
         </div>
 
-        <div className="grid grid-cols-[1fr_2rem_1fr] p-4">
-            <TransferCargoItem ship={ship}/>
-            <div></div>
-            <TransferCargoItem ship={ship}/>
-        </div>
-    </div>
+        {!availableShips.length && <div className="p-4 text-center text-lg">
+            <p>No ships available. </p>
+            <p>Ships must be in the same status for transfer.</p>
+        </div>}
+
+        {!selectedShip.symbol && !!availableShips.length && 
+            <ul className="h-overflow p-4 text-xs grid grid-cols-2 gap-4">
+                {availableShips.map(item => 
+                <SelectShip key={item.symbol} ship={item} onSelect={setSelectedShip}/>)}
+            </ul>
+        }
+
+        {selectedShip.symbol && <div className="grid grid-cols-[1fr_2rem_1fr]">
+            <TransferCargoItem setSelectedShip={setSelectedShip} ship={ship} secondShip={selectedShip}/>
+            <span></span>
+            <TransferCargoItem setSelectedShip={setSelectedShip} ship={selectedShip} secondShip={ship}
+            direction="right"/>
+        </div>}
+
+        {selectedShip.symbol && <BackButton onClick={() => setSelectedShip({})}/>}
+    </div>)
 }
