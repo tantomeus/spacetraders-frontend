57054321b95c3b9e12a11ec19905d018dbdba438
Stormblessed <carlomanid@gmail.com>
2023-09-27 23:21:09 +0300

    Add handlers

--- src/components/Marketplace.js
+++ src/components/Marketplace.js
@@ -3,29 +3,58 @@
 import { useAccount } from "@/context/AccountContext";
 import { trade, viewMarketplace } from "@/services/api";
 import { useEffect, useState } from "react";
-import MarketplaceItem from "./MarketplaceItem";
-import ShipImg from "./ShipImg";
 import { shorten } from "@/helpers/helpers";
-import { useShips } from "@/context/ShipsContext";
+
+import MarketplaceItem from "./MarketplaceItem";
+import SelectShip from "./SelectShip";
+import BackButton from "./BackButton";
+import SkeletonLoader from "./SkeletonLoader";
+
+const errorMessage = "I missed the part where that's my problem";
 
 export default function Marketplace({ system, waypoint, ships }) {
-    const { account, setAccount } = useAccount();
-    const { setShips } = useShips();
-    const [goods, setGoods] = useState({});
+    const { account, setAccount, setShips, notify } = useAccount();
     const [status, setStatus] = useState(""); // purchase, sell
-    const [selectedShip, setSelectedShip] = useState(null);
-    const [selectedGoods, setSelectedGoods] = useState(null);
+    const [goods, setGoods] = useState({});
+    const [selectedShip, setSelectedShip] = useState({});
+    const [selectedGoods, setSelectedGoods] = useState({});
     const [amount, setAmount] = useState(0);
+    const [isLoading, setIsLoading] = useState(true);
 
     const availableShips = ships.filter(ship => ship.cargo.capacity && ship.nav.status === "DOCKED")
 
-    async function handleTrade(credits) {
-        const data = await trade(account.token, selectedShip.symbol, selectedGoods.symbol, amount, status);
-        setAccount((account) => ({...account, credits: data.agent.credits}));
-        setSelectedShip((ship) => ({...ship, cargo: data.cargo}));
-        setShips((ships) => ships.map((ship) => ship.symbol === selectedShip.symbol ? selectedShip : ship));
-        setSelectedGoods(() => null);
+    function reset() {
+        setSelectedGoods({});
         setAmount(0);
+    }
+
+    async function handleTrade() {
+        try {
+            const data = await trade(account.token, selectedShip.symbol, selectedGoods.symbol, amount, status);
+
+            if (!data) throw new Error(errorMessage);
+
+            setAccount((account) => ({...account, credits: data.agent.credits}));
+            setSelectedShip((ship) => ({...ship, cargo: data.cargo}));
+            setShips((ships) => ships.map((ship) => ship.symbol === selectedShip.symbol
+            ? {...ship, cargo: data.cargo}
+            : ship));
+            reset();
+        } catch (err) {
+            notify(err.message);
+        }
+    }
+
+    function handleInputChange(e) {
+        const space = status === "sell"
+        ? selectedGoods.tradeVolume
+        : selectedShip.cargo.capacity - selectedShip.cargo.units ;
+        const min = Math.floor(Math.min((account.credits / selectedGoods.purchasePrice), space));
+        
+        if (!isNaN(+e.target.value)) setAmount(+(e.target.value));
+        if (e.target.value * selectedGoods.purchasePrice > account.credits || e.target.value > space) {
+            setAmount(min);
+        }
     }
 
     function handleSelect(item, type) {
@@ -34,22 +63,29 @@ export default function Marketplace({ system, waypoint, ships }) {
     }
 
     useEffect(() => {
-        async function fetching() {
-            const data = await viewMarketplace(account.token, system, waypoint);
-            setGoods(data);
+        async function fetchData() {
+            setIsLoading(true);
+            try {
+                const data = await viewMarketplace(account.token, system, waypoint);
+                if (!data) throw new Error(errorMessage);
+                setGoods(data);
+            } catch (err) {
+                notify(err.message);
+            } finally {
+                setIsLoading(false);
+            }
         }
-        fetching();
-    }, [account.token, system, waypoint]);
+        fetchData();
+    }, [account.token, system, waypoint, notify]);
     
-    if (selectedGoods) return (
+    if (selectedGoods.symbol) return (
         <div className="window window-divide w-[30rem]">
-            <div className="flex justify-between items-center px-6 py-4 text-2xl">
+            <div className="flex-between px-6 py-4 text-2xl">
                 <h2>Marketplace</h2>
-                <span>{selectedShip?.symbol}</span>
             </div>
 
             <div className="p-4">
-                <div className="flex justify-between items-center bg-stone-700 px-4 py-2 rounded-md">
+                <div className="flex-between bg-stone-700 px-4 py-2 rounded-primary">
                     <div>
                         <span className="block">{selectedGoods.name}</span>
                         <span className="block">{selectedGoods.tradeVolume} Available to {status}</span>
@@ -58,43 +94,57 @@ export default function Marketplace({ system, waypoint, ships }) {
                 </div>
 
                 <div className="mt-4">
-                    <input value={amount} onChange={(e) => {
-                        const space = status === "sell" ? selectedGoods.tradeVolume : selectedShip.cargo.capacity - selectedShip.cargo.units ;
-                        if (!isNaN(+e.target.value)) setAmount(+(e.target.value));
-                        if (e.target.value > space) setAmount(+space);
-                    }} placeholder="Amount" className="grow bg-transparent border-stone-700 border rounded-md px-3 py-3 w-full"/>
+                    <input value={amount} onChange={handleInputChange} placeholder="Amount"
+                    className="grow input py-3 w-full"/>
                 </div>
             </div>
 
             <div className="flex justify-between px-4 py-4">
-                <button onClick={() => setSelectedGoods(null)}>&larr; BACK</button>
-                <button onClick={handleTrade} className="btn-color hover:btn-color-hover">{status} FOR {amount * selectedGoods.price} CREDITS</button>
+                <BackButton onClick={reset}/>
+                <button onClick={handleTrade}
+                className="btn btn-color hover:btn-color-reversed">
+                    {status} FOR {amount * selectedGoods.price} CREDITS
+                </button>
             </div>
         </div>
     );
 
     return <div className="window window-divide w-[60rem]">
-        <div className="flex justify-between items-center px-6 py-4 text-2xl">
+        <div className="flex-between px-6 py-4 text-2xl">
             <h2>Marketplace</h2>
-            <span className="credits rounded-3xl py-2 px-4 text-xl">{shorten(account.credits)} credits</span>
-            {selectedShip?.symbol}
+            {selectedShip.symbol && <>
+                <span>{selectedShip.symbol}</span>
+                <span>{selectedShip.cargo.units}/{selectedShip.cargo.capacity}</span>
+                <span className="credits rounded-3xl py-2 px-4 text-xl">
+                    {shorten(account.credits)} credits
+                </span>
+            </>}
         </div>
 
-        {availableShips.length ? <ul className="overflow-auto max-h-[70vh] p-4 text-xs grid grid-cols-2 gap-4">
+        {availableShips.length
+        ? <ul className="h-overflow p-4 text-xs grid items-start grid-cols-2 gap-4">
 
-            {!selectedShip && availableShips.map(ship => ship.cargo.capacity ? <li key={ship.symbol} className="flex items-center gap-6 p-4 border-solid border border-stone-500 rounded-md">
-                <ShipImg ship={ship}/>
-                <h3 className="font-medium">{ship.symbol}</h3>
-                <div className="relative ml-auto">
-                    <button onClick={() => setSelectedShip(ship)} className="btn-color hover:btn-color-hover text-xs/[1rem]">SELECT</button>
-                </div>
-            </li> : "")}
+            {!selectedShip.symbol && availableShips.map(ship => ship.cargo.capacity
+                ? <SelectShip key={ship.symbol} ship={ship} onSelect={setSelectedShip}>
+                    {ship.cargo.units}/{ship.cargo.capacity}
+                </SelectShip>
+                : "")
+            }
 
-            {selectedShip && goods.tradeGoods?.map((item) => <MarketplaceItem onSelect={handleSelect} ship={selectedShip} item={item} key={item.symbol}/>)}
-        </ul> : <p className="uppercase py-8 text-center text-xl">There are no docked ships with a cargo module</p>}
+            {isLoading && selectedShip.symbol && ["", ""].map((_, i) => <SkeletonLoader key={i}/>)}
 
-        {selectedShip && <div className="px-6 py-4">
-            <button onClick={() => setSelectedShip(null)}>&larr; BACK</button>
-        </div>}
+            {!isLoading && selectedShip.symbol && goods.tradeGoods?.map((item) =>
+            <MarketplaceItem
+            account={account}
+            onSelect={handleSelect}
+            ship={selectedShip}
+            item={item}
+            key={item.symbol}
+            />)}
+
+        </ul>
+        : <p className="uppercase py-8 text-center text-xl">There are no docked ships with a cargo module</p>}
+
+        {selectedShip.symbol && <BackButton onClick={reset}/>}
     </div>
 }
--- src/components/MarketplaceItem.js
+++ src/components/MarketplaceItem.js
@@ -1,31 +1,64 @@
 import { shorten } from "@/helpers/helpers";
 
-export default function MarketplaceItem({ item, ship, onSelect }) {
+export default function MarketplaceItem({ item, ship, onSelect, account }) {
 
-    const name = item.symbol.toLowerCase().split("_").map(item => item[0].toUpperCase() + item.slice(1)).join(" ");
+    const name = item.symbol.toLowerCase().split("_")
+    .map(item => item[0].toUpperCase() + item.slice(1)).join(" ");
     const cargo = ship?.cargo?.inventory.find((cargo) => cargo.symbol === item.symbol);
+    const ableToBuy = ship.cargo.capacity !== ship.cargo.units && account.credits >= item.purchasePrice;
 
-    return <li className="border-solid border border-stone-500 rounded-md text-sm p-4 space-y-4">
-        <div className="flex justify-between items-center gap-2">
+    return (
+    <li className="grey-border rounded-primary text-sm p-4 space-y-4">
+        <div className="flex-between gap-2">
             <h3 className="text-lg">{name}</h3>
         </div>
 
         <hr className="grow opacity-50"/>
 
-        <div className="flex justify-between items-center gap-2">
-            <span className="text-stone-700 bg-amber-400 text-xs/[1rem] p-2 rounded-md">{shorten(item.purchasePrice)} CREDITS</span>
+        <div className="flex-between gap-2">
+            <span
+            className="credits text-xs/[1rem] p-2 rounded-primary">
+                {shorten(item.purchasePrice)} CREDITS
+            </span>
+
             <hr className="block grow opacity-50"/>
-            <span className="bg-stone-500 text-xs/[1rem] p-2 rounded-md">{shorten(item.tradeVolume)}</span>
+
+            <span
+            className="bg-stone-500 text-xs/[1rem] p-2 rounded-primary">
+                {shorten(item.tradeVolume)}
+            </span>
+
             <hr className="block grow opacity-50"/>
-            <button onClick={() => onSelect({...item, name, price: item.purchasePrice}, "purchase")} className="btn-color hover:btn-color-hover text-xs/[1rem]">purchase</button>
+
+            <button
+            onClick={() => onSelect({...item, name, price: item.purchasePrice}, "purchase")}
+            disabled={!ableToBuy}
+            className={`btn ${ableToBuy
+            ? "btn-color hover:btn-color-reversed"
+            : "disable-color"} text-xs/[1rem]`}>
+                purchase
+            </button>
         </div>
 
-        <div className="flex justify-between items-center gap-2">
-            <span className="text-stone-700 bg-amber-400 text-xs/[1rem] p-2 rounded-md">{shorten(item.sellPrice)} CREDITS</span>
+        <div className="flex-between gap-2">
+            <span className="credits text-xs/[1rem] p-2 rounded-primary">
+                {shorten(item.sellPrice)} CREDITS
+            </span>
+
             <hr className="block grow opacity-50"/>
-            <span className="bg-stone-500 text-xs/[1rem] p-2 rounded-md">{shorten(cargo?.units) || 0}</span>
+            
+            <span className="bg-stone-500 text-xs/[1rem] p-2 rounded-primary">
+                {shorten(cargo?.units) || 0}
+            </span>
             <hr className="block grow opacity-50"/>
-            <button onClick={() => onSelect({...item, name, tradeVolume: cargo.units, price: item.sellPrice}, "sell")} disabled={!cargo} className={`${cargo ? "hover:btn-color-hover" : "bg-stone-700"} btn-color text-xs/[1rem]`}>SELL</button>
+            <button
+            onClick={() => onSelect({...item, name, tradeVolume: cargo.units, price: item.sellPrice}, "sell")}
+            disabled={!cargo}
+            className={`btn ${cargo
+            ? "btn-color hover:btn-color-reversed"
+            : "disable-color"} text-xs/[1rem]`}>
+                SELL
+            </button>
         </div>
-    </li>
+    </li>)
 }
