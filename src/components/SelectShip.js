b1e7b1080b7d8a84d7bf9e9d59e000a5b8328a14
Stormblessed <carlomanid@gmail.com>
2023-09-27 23:17:09 +0300

    Add children

--- src/components/SelectShip.js
+++ src/components/SelectShip.js
@@ -1,15 +1,18 @@
 import ShipImg from "./ShipImg";
 
-export default function SelectShip({ ship, onSelect }) {
+export default function SelectShip({ ship, onSelect, children }) {
     
     return (
-    <li 
-    className="flex items-center gap-6 p-4 grey-border rounded-md">
-        <ShipImg ship={ship}/>
-        <h3 className="text-md">{ship.symbol}</h3>
-        <div className="relative ml-auto">
-            <button onClick={() => onSelect(ship)}
-            className="btn btn-color hover:btn-color-reversed text-xs/[1rem]">SELECT</button>
+    <li className="flex-between p-4 grey-border rounded-primary item-hover-color">
+        <div className="flex-between gap-4">
+            <ShipImg ship={ship}/>
+            <h3 className="text-md">{ship.symbol}</h3>
         </div>
+
+        <span>{children}</span>
+        
+        <button 
+        onClick={() => onSelect(ship)}
+        className="btn btn-color hover:btn-color-reversed text-xs/[1rem]">SELECT</button>
     </li>)
 }
