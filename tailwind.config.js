c7080525c285b3e46f582c8c8e006573da33e2e1
tantomeus <carlomanid@gmail.com>
2023-11-12 18:21:48 +0300

    Add custom breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -41,6 +41,15 @@ module.exports = {
         "skeleton": "skeletonLoading 1s linear infinite alternate"
       }
     },
+    screens: {
+      "2xs": "375px",
+      "xs": "425px",
+      "sm": "640px",
+      "md": "768px",
+      "lg": "1024px",
+      "xl": "1280px",
+      "2xl": "1536px",
+    },
     fontFamily: {
       'speedy': ['Speedy']
     }
