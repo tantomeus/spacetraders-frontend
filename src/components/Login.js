3fd3c6372829bcfb219336091bb78f4f2d722123
tantomeus <carlomanid@gmail.com>
2023-11-19 21:26:31 +0300

    Add adaptation

--- src/components/Login.js
+++ src/components/Login.js
@@ -88,8 +88,8 @@ export default function Login() {
 
     return (
     <div style={{boxShadow: "0 11px 15px -7px rgba(0,0,0,.2), 0 24px 38px 3px rgba(0,0,0,.14), 0 9px 46px 8px rgba(0,0,0,.12)"}}
-    className="window w-[30rem] overflow-hidden">
-        <div className="grid grid-cols-3 relative">
+    className="window w-[18rem] sm:w-[30rem] overflow-hidden">
+        <div className="relative grid grid-cols-3 text-sm sm:text-base">
             <button
             onClick={() => setCurrentTab("switch")}
             className={`py-4 grow uppercase transition-primary ${switchAcc ? activeClass : "item-hover-color"}`}>
@@ -107,44 +107,45 @@ export default function Login() {
             className={`py-4 grow uppercase transition-primary ${create ? activeClass : "item-hover-color"}`}>
                 Create
             </button>
-            {<span className={`absolute left-0 bottom-0 bg-amber-600 h-0.5 w-40 ${moving} transition-primary`}></span>}
+            
+            {<span className={`absolute left-0 bottom-0 bg-amber-600 h-0.5 w-[6rem] sm:w-40 ${moving} transition-primary`}></span>}
         </div>
 
-        <div className={`grid grid-cols-[30rem_30rem_30rem] ${height} transition-primary`}>
+        <div className={`grid grid-cols-[18rem_18rem_18rem] sm:grid-cols-[30rem_30rem_30rem] ${height} transition-primary`}>
             <ul className={`h-overflow ${movingWindow} transition-primary`}>
                 {local.length
                 ? local.map((agent) =>
-                <li className="cursor-pointer p-5 rounded-primary item-hover-color flex-between"
+                <li className="p-5 cursor-pointer rounded-primary item-hover-color flex-between"
                 key={agent.token} onClick={(e) => handleAuth(e, agent.token)}>
                     <span>{agent.name}</span>
 
                     <button onClick={(e) => handleRemove(e, agent.token)}>
-                        <FaTrashAlt className="hover:fill-amber-600 h-6 w-6"/>
+                        <FaTrashAlt className="w-6 h-6 hover:fill-amber-600"/>
                     </button>
                 </li>)
-                : <li className="p-5 flex justify-center items-center translate-y-full">NO ACCOUNTS</li>}
+                : <li className="flex items-center justify-center p-5 translate-y-full">NO ACCOUNTS</li>}
             </ul>
 
             <form className={`flex flex-col justify-start gap-8 p-5 ${movingWindow} transition-primary`}
             onSubmit={(e) => handleAuth(e, token)}>
                 <div className="relative">
                     <input value={token} onChange={(e) => setToken(e.target.value)}
-                    className="input py-3 w-full peer"/>
+                    className="w-full py-3 input peer"/>
                     <label className={`floating-label peer-focus:translate-y-[-170%] ${token ? "translate-y-[-170%] scale-[80%] text-stone-50 z-10" : ""}`}>Token</label>
                 </div>
-                <button className="btn btn-color hover:btn-color-reversed text-xl">Sign In</button>
+                <button className="text-xl btn btn-color hover:btn-color-reversed">Sign In</button>
             </form>
 
-            <form onSubmit={handleSignUp} className={`flex flex-col justify-start gap-8 p-5 ${movingWindow} transition-primary`}>
+            <form onSubmit={handleSignUp} className={`flex flex-col justify-start gap-4 sm:gap-8 p-5 ${movingWindow} transition-primary`}>
                 <div className="relative">
                     <input value={username} onChange={(e) => setUsername(e.target.value)}
-                    className="input py-3 w-full peer"/>
+                    className="w-full py-3 input peer"/>
                     <label className={`floating-label peer-focus:translate-y-[-170%] ${username ? "translate-y-[-170%] scale-[80%] text-stone-50 z-10" : ""}`}>Username</label>
                 </div>
 
-                <div className="flex justify-between gap-2">
+                <div className="flex flex-col justify-between gap-2 sm:flex-row">
                     <select
-                    className="input py-3 grow"
+                    className="py-3 text-[10px] input grow sm:text-base"
                     onChange={(e) => setSelectedFaction(e.target.value)}
                     value={selectedFaction}>
                         {factions.map(faction =>
@@ -158,12 +159,13 @@ export default function Login() {
 
                     <button
                     onClick={() => setIsFactionInfoOpen(true)}
-                    type="button">
-                        <AiOutlineInfoCircle className="icon-size-primary rounded-full hover:fill-amber-600"/>
+                    type="button"
+                    className="flex items-center justify-center">
+                        <AiOutlineInfoCircle className="hidden rounded-full sm:block icon-size-primary hover:fill-amber-600"/>
                     </button>
                 </div>
                 
-                <button className="btn btn-color hover:btn-color-reversed text-xl">Sign Up</button>
+                <button className="text-xl btn btn-color hover:btn-color-reversed">Sign Up</button>
             </form>
         </div>
 
